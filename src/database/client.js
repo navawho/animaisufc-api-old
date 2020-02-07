9602af369acac4d4fe79f272a78ca2a134269782
navarrotheus <matheusnavarro@alu.ufc.br>
2020-02-07 16:16:17 -0300

    [~] heroku auth

--- src/database/client.js
+++ src/database/client.js
@@ -4,7 +4,7 @@ const pg = require('pg');
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-const isProduction = `${process.env.NODE_ENV}` === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const client = new pg.Client({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
