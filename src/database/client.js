f868d6a6dfedb791dd7d57a14650c8d192799770
navarrotheus <matheusnavarro@alu.ufc.br>
2020-02-07 15:53:12 -0300

    [~] switching to string connection

--- src/database/client.js
+++ src/database/client.js
@@ -2,16 +2,13 @@ require('dotenv/config');
 
 const pg = require('pg');
 
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const client = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  define: {
-    underscored: true,
-    underscoredAll: true,
-  },
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
 });
 
 module.exports = client;
