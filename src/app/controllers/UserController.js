c6df7f6930ddf7b85b6daa01534cef57f4df3dba
navarrotheus <matheusnavarro@alu.ufc.br>
2020-02-14 09:01:02 -0300

    [+] Password Hash

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,9 @@
+import bcrypt from 'bcryptjs';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const { name, email, password_hash } = req.body;
+    const { name, email, password } = req.body;
 
     const userExists = await User.getUserByEmail(email);
 
@@ -11,6 +12,8 @@ class UserController {
         .status(400)
         .json({ error: 'O e-mail especificado já está cadastrado.' });
     }
+
+    const password_hash = await bcrypt.hash(password, 8);
 
     const { rows } = await User.create(name, email, password_hash);
 
