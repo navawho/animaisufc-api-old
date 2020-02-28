c6206eb837b21f789c22b68927342bb7a2f54276
navarrotheus <matheusnavarro@alu.ufc.br>
2020-02-27 22:28:18 -0300

    [+] User CRUD

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
+import authAdminMiddleware from './app/middlewares/authAdmin';
 
 import FileController from './app/controllers/FileController';
 import AnimalController from './app/controllers/AnimalController';
@@ -16,17 +17,23 @@ routes.get('/animals', AnimalController.index);
 routes.get('/animals/:id', AnimalController.indexById);
 routes.get('/dogs', AnimalController.indexDogs);
 routes.get('/cats', AnimalController.indexCats);
-routes.post('/animals', AnimalController.store);
 
 routes.get('/files', FileController.index);
 routes.get('/files/:id', upload.single('file'), FileController.indexById);
-routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.get('/users', UserController.indexById);
 routes.put('/users', UserController.update);
+routes.delete('/users', UserController.remove);
+
+routes.use(authAdminMiddleware);
+
+routes.post('/animals', AnimalController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
