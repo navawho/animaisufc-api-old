65c4456dbc7804b7e744cfe53168d332386c23d2
navarrotheus <matheusnavarro@alu.ufc.br>
2020-03-10 17:48:47 -0300

    ~ revert

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
-import authAdminMiddleware from './app/middlewares/authAdmin';
 
 import FileController from './app/controllers/FileController';
 import AnimalController from './app/controllers/AnimalController';
@@ -17,18 +16,18 @@ routes.get('/animals', AnimalController.index);
 routes.get('/animals/:id', AnimalController.indexById);
 routes.get('/dogs', AnimalController.indexDogs);
 routes.get('/cats', AnimalController.indexCats);
+routes.post('/animals', AnimalController.store);
 
 routes.get('/files', FileController.index);
 routes.get('/files/:id', upload.single('file'), FileController.indexById);
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/sessions', SessionController.store);
 
 routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
-routes.use(authAdminMiddleware);
 
-routes.get('/users', UserController.indexById);
 routes.put('/users', UserController.update);
 routes.delete('/users', UserController.remove);
 
